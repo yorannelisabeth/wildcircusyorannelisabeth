5d2b67def16eb0b5e9e54ef975b989351d21b31b
yorannelisabeth <yorannelisabeth@gmail.com>
2019-07-16 22:08:10 +0200

    link created

--- yorannwildcircus/src/app/app.module.ts
+++ yorannwildcircus/src/app/app.module.ts
@@ -7,10 +7,50 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LocationComponent } from './location/location.component';
+import { RouterModule, Routes } from "@angular/router";
 import { PriceComponent } from './price/price.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactusComponent } from './contactus/contactus.component';
+
+
+var routes: Routes = [
+  {
+    path: "",
+    component: AppComponent,
+    children: [
+      {
+        path: "home",
+        pathMatch: "full",
+        component: HomeComponent
+      },
+      {
+        path: "gallery",
+        pathMatch: "full",
+        component: GalleryComponent
+      },
+      {
+        path: "price",
+        pathMatch: "full",
+        component: PriceComponent
+      },
+     
+      
+      {
+        path: "location",
+        pathMatch: "full",
+        component: LocationComponent
+      },
+      {
+        path: "contactus",
+        pathMatch: "full",
+        component: ContactusComponent
+      },
+    ]
+  },
+
+  
+];
 
 
 @NgModule({
@@ -30,6 +70,16 @@ import { ContactusComponent } from './contactus/contactus.component';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    RouterModule.forRoot(
+      routes,
+      {
+        useHash: false,
+        enableTracing: false,
+        anchorScrolling: "enabled",
+        onSameUrlNavigation: "reload",
+        scrollPositionRestoration: "enabled",
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
