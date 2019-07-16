1ecfe68c0feefca5f1f344aef7ea4a16d5621797
yorannelisabeth <yorannelisabeth@gmail.com>
2019-07-16 20:24:35 +0200

    addprice component

--- yorannwildcircus/src/app/app.module.ts
+++ yorannwildcircus/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LocationComponent } from './location/location.component';
+import { PriceComponent } from './price/price.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    LocationComponent
+    LocationComponent,
+    PriceComponent
   ],
   imports: [
     BrowserModule,
