ded67fae4a77ced58a278d65960a78625df1bccd
yorannelisabeth <yorannelisabeth@gmail.com>
2019-07-16 16:42:11 +0200

    create footer

--- yorannwildcircus/src/app/app.module.ts
+++ yorannwildcircus/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
