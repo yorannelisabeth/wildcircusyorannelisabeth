72f5230a53e99f497caed6492d1faa4b011e089f
yorannelisabeth <yorannelisabeth@gmail.com>
2019-07-17 08:53:11 +0200

    create back files

---
+++ back/wildcircusback/src/main/java/com/wildcircusyorann/wildcircusback/WildcircusbackApplication.java
@@ -0,0 +1,13 @@
+package com.wildcircusyorann.wildcircusback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcircusbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildcircusbackApplication.class, args);
+	}
+
+}
